1507c33ecd15d64cb79c926ca9426cbd8e896545
Eric Frommelt <ericfrommelt@gmail.com>
2025-09-24 15:37:49 -0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,23 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import { astroImageTools } from 'astro-imagetools';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), astroImageTools],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
