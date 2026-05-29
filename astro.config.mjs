061b282eaa2091334080eddebd4dea8be10ac907
Eric Frommelt <ericfrommelt@gmail.com>
2026-05-29 10:46:07 -0700

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://ericfrommelt.com',
   integrations: [mdx(), sitemap(), astroImageTools],
 
   vite: {
