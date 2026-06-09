95cab16b3c0f128bb003199fea4b6d98b49394d8
Eric Frommelt <ericfrommelt@gmail.com>
2026-06-08 21:33:34 -0700

    edits, fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import { astroImageTools } from 'astro-imagetools';
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Eric Frommelt - Motion Designer';
-export const SITE_DESCRIPTION = 'Motion Designer based in Los Angeles, California';
+export const SITE_TITLE = 'Eric Frommelt - Graphic Artist + Technologist';
+export const SITE_DESCRIPTION = 'Graphic artist + technologist based in Los Angeles, California';
