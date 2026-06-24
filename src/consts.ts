854da05e53f3518659a1b5a9bb3fa7defd977477
Eric Frommelt <ericfrommelt@gmail.com>
2026-06-24 11:09:58 -0700

    updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Eric Frommelt - Graphic Artist + Technologist';
-export const SITE_DESCRIPTION = 'Graphic artist + technologist based in Los Angeles, California';
+export const SITE_TITLE = 'Eric Frommelt - Graphic Designer + Motion Illustrator';
+export const SITE_DESCRIPTION = 'Graphic Designer + Illustrator based in Los Angeles, California';
+export const SITE_INTRO = 'Hi, I\'m Eric Frommelt. A graphic designer and illustrator based in Los Angeles, California. I make things like editorial illustrations, style frames for motion design, generative illustration systems, and album artwork. My style is bold, colorful, and experimental, mixing Swiss grid mechanics with digital abstract expressionism — shot through the lens of technology.';
